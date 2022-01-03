91df6d835efd56fd791377c52abf68ebbd1bc438
vitor barbosa de menezes <vitormenezes43@icloud.com>
2022-01-03 18:17:25 -0300

    update

--- exemploPOO/src/exemplopoo/ExemploPOO.java
+++ exemploPOO/src/exemplopoo/ExemploPOO.java
@@ -3,18 +3,12 @@ public class ExemploPOO {
 
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
+        c1.modelo = "BIc cristal";
         c1.cor = "Azul";
-        c1.ponta = 0.5f;
-        c1.tampar();
-        c1.rabiscar(); 
+        //c1.ponta = 0.5f;
+        c1.carga = 80;
         c1.status();
-        
-        Caneta c2 = new Caneta();
-        c2.modelo = "bic";
-        c2.cor = "preta";
-        c2.destampar();
-        c2.rabiscar();
-        c2.status();
+
         
     }
     

82edba48e4e995ff3d7e5ec59214e7a92a7f7297
vitor barbosa de menezes <vitormenezes43@icloud.com>
2022-01-17 18:06:52 -0300

    17/01

---
+++ treinamento01/src/treinamento01/Publicacao.java
@@ -0,0 +1,9 @@
+package treinamento01;
+public interface Publicacao {
+    public void abrir();
+    public void fechar();
+    public void folhear(int p);
+    public void avacarPag();
+    public void voltarPag();
+        
+}
